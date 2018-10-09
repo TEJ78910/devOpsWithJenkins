225a6afe7255a05c7001d6d2b516ffeb5ab9d2cf
tejas <tejasinamdar89@gmail.com>
2018-10-09 12:49:18 +0530

    first commit with jenkins

---
+++ src/main/java/com/siemens/myAppWithJenkins/MyAppWithJenkinsApplication.java
@@ -0,0 +1,12 @@
+package com.siemens.myAppWithJenkins;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MyAppWithJenkinsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MyAppWithJenkinsApplication.class, args);
+	}
+}
